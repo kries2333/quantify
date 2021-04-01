b00b3ba07f9c0b17337cefc5d50095979c4c90fe
kries <kries2333@gmail.com>
2021-04-01 09:55:55 +0800

    添加永续合约k线获取

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from save_okex import QA_SU_save_okex
+from save_okex_spot import QA_SU_save_okex
+from save_okex_swap import QA_SU_save_okex_swap
 
 if __name__ == '__main__':
-    QA_SU_save_okex()
+    # QA_SU_save_okex()
+    QA_SU_save_okex_swap()
